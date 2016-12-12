0e8ba61bfd415817e083a84384245db1d0646f3d
Shamil <badalloffsh@gmail.com>
2016-12-12 08:26:53 +0400

    Add: user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe User, type: :model do
     it { should have_one(:profile).dependent(:destroy)}
     it { should accept_nested_attributes_for (:profile)}
   end
+
+  describe 'Validations' do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+  end # Validations
+
 end
