1277d05ae6dd6ce13fe2fb3828beef5162dd1d53
Shamil <badalloffsh@gmail.com>
2016-12-12 08:25:47 +0400

    Add: user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
-  #
-  # it 'has a counter cache' do
-  #   expect {
-  #     user.event.create(:event)
-  #   }.to change { User.last.event_count }.by(1)
-  # end
+  describe 'Associations' do
+    it { should have_many(:events).dependent(:destroy)}
+    it { should have_many(:event_memberships).dependent(:destroy)}
+    it { should have_one(:profile).dependent(:destroy)}
+    it { should accept_nested_attributes_for (:profile)}
+  end
 end
