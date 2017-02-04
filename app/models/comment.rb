a5613fe0a07c870ff808d8dcda1bd13c6c56028f
Shamil <badalloffsh@gmail.com>
2017-02-04 18:37:23 +0400

    Add: validation to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, :polymorphic => true
+
+  validates_presence_of :body
+  validates :body, length: { maximum: 2000 }
 end
