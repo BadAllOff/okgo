49ac5140e3fd9cf37c6c6eab01bea721edb0451d
Shamil <badalloffsh@gmail.com>
2017-02-04 09:38:46 +0400

    Add: changed styling and translations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_comment, only: [:destroy]
+  before_action :set_comment, only: [:update, :destroy]
 
   authorize_resource
 
