c9c61b6117f1fcb6202729ea9da1f216fccdd186
Shamil <badalloffsh@gmail.com>
2017-02-14 13:12:12 +0400

    Add: pagination to feedbacks

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -12,7 +12,7 @@ class FeedbacksController < ApplicationController
   # GET /feedbacks
   # GET /feedbacks.json
   def index
-    @feedbacks = Feedback.all.order(created_at: 'desc').includes(:comments, user: [:profile])
+    @feedbacks = Feedback.order(created_at: 'desc').includes(:comments, user: [:profile]).page(params[:page]).per(20)
   end
 
   # GET /feedbacks/1
