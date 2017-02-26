ab4b2e7a53e454812f02bd3f43e797bca419aaed
Shamil <badalloffsh@gmail.com>
2017-02-27 01:39:11 +0400

    Add: notification metod, set notices as read

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,9 +4,18 @@ class ActivitiesController < ApplicationController
   include Breadcrumbed
   before_action :set_activity_index_breadcrumb
   before_action :set_page_title
+  before_action :get_notices
+  after_action :reset_notices_counter
+  after_action :set_notifications_as_read
 
   def index
-    @notices = Notice.where(user: current_user).order('created_at desc').limit(10).pluck(:activity_id)
+    @activities = PublicActivity::Activity.where(id: @notices).order('created_at desc').includes(:trackable, :owner)
+    respond_to do |format|
+      format.html { @activities }
+    end
+  end
+
+  def notifications
     @activities = PublicActivity::Activity.where(id: @notices).order('created_at desc').includes(:trackable, :owner)
   end
 
@@ -18,5 +27,17 @@ class ActivitiesController < ApplicationController
 
   def set_page_title
     @page_title = I18n.t('page_titles.activities')
+  end
+
+  def reset_notices_counter
+    User.reset_counters(current_user.id, :notices)
+  end
+
+  def get_notices
+    @notices = Notice.where(user: current_user).order('created_at desc').limit(10).pluck(:activity_id)
+  end
+
+  def set_notifications_as_read
+    Notice.where(user: current_user, read: false).update(read_at: Time.zone.now, read: true)
   end
 end
