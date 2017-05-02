897128c3773c8c1dde91f95411bbbfea61b14367
Shamil <badalloffsh@gmail.com>
2017-05-02 22:43:25 +0400

    Add: simple test for lang

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  describe 'Associations' do
+
+  end
+
+  describe 'Validations' do
+
+  end
+end
---
+++ spec/models/language_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Language, type: :model do
+  describe 'Associations' do
+
+  end
+
+  describe 'Validations' do
+    # it { should validate_presence_of :description }
+    it { expect(Language.new()).to_not  be_valid }
+  end
+end
