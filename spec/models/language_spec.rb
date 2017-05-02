249e1f965f4f0c629428ccddb58a068f1180c29e
Shamil <badalloffsh@gmail.com>
2017-05-02 22:44:46 +0400

    Add: simple test for lang

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Language, type: :model do
   end
 
   describe 'Validations' do
-    it { should validate_presence_of :description }
-    # it { expect(Language.new()).to_not  be_valid }
+    # it { should validate_presence_of :description }
+    it { expect(Language.new()).to_not  be_valid }
   end
 end
