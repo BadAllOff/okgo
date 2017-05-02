045616476ea95d9189e6daefb113487e82acba8f
Shamil <badalloffsh@gmail.com>
2017-05-02 23:04:19 +0400

    Add: simple event spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,6 +6,24 @@ RSpec.describe Event, type: :model do
   end
 
   describe 'Validations' do
+    it { should validate_presence_of :max_members }
+    it { should validate_presence_of :language }
+    it { should validate_numericality_of(:latitude) }
+    it { should validate_numericality_of(:longitude) }
 
+    it do
+      should validate_length_of(:title).
+          is_at_least(4).is_at_most(140)
+    end
+
+    it do
+      should validate_length_of(:description).
+          is_at_least(10).is_at_most(3000)
+    end
+
+    it do
+      should validate_length_of(:address).
+          is_at_least(10).is_at_most(255)
+    end
   end
 end
